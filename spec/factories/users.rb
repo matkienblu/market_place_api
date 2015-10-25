b7b0e13444a0495cec73c7a31cbc0bd0facee352
matkienblu <binh.it271@gmail.com>
2015-10-25 17:06:26 +0700

    adds user first spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    # this is a mock data - prepared by Test DB
+    email {FFaker::Internet.email }
+    password "12345678"
+    password_confirmation "12345678"
   end
 
 end
