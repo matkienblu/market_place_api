3d2a19899af57114b5d13e62b43ae4ecde4445bd
matkienblu <binh.it271@gmail.com>
2015-10-26 16:48:17 +0700

    Adds the user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,19 +1,63 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
-
-  describe "GET #show" do
+  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
+  # test for getting and user
+  describe 'GET #show' do
+    # create mock data before testing
     before(:each) do
       @user = FactoryGirl.create :user
       get :show, id: @user.id, format: :json
     end
 
-    it "returns the information about a reporter on a hash" do
+    # check email in mock and data from getting are the same
+    it 'returns the information about a reporter on a hash' do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
-
+    # check status code
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+
+    context 'when is successfullly created' do
+      # create mock random data for testing
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it 'renders the json representation for the user record just created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when is not created' do
+      before(:each) do
+        #notice I'm not including the email
+        @invalid_user_attributes = { password: '12345678',
+                                     password_confirmation: '12345678' }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on why the user could not be created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+
+
+  end
+
 end
