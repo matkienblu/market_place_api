d53f072fc837099cfa2a00acebb3cfad9b0a1492
matkienblu <binh.it271@gmail.com>
2015-11-02 11:33:48 +0700

    adds set total

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ActiveRecord::Base
   # validata data
   validates :total,numericality: {greater_than_or_equal_to: 0}, presence: true
   validates :user_id, presence: true
+  validates_with EnoughProductsValidator
 
   # relational
   belongs_to :user
@@ -12,14 +13,17 @@ class Order < ActiveRecord::Base
   before_validation :set_total!
 
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = 0
+    placements.each do |placement|
+      self.total += placement.product.price * placement.quantity
+    end
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
     product_ids_and_quantities.each do |product_id_and_quantity|
       id, quantity = product_id_and_quantity # [1,5]
 
-      self.placements.build(product_id: id)
+      self.placements.build(product_id: id, quantity: quantity)
     end
   end
 
